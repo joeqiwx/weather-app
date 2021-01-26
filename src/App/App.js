08b6d967e04902d9db458f7ef1433361d0ab3eca
Weixin Qi <s4590288@gmail.com>
2021-01-26 23:01:09 +1000

    feat: 🎸 finish the static componet app and relative scss file

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,13 @@
+import './App.scss';
+import Header from './components/Header/Header'
 import Content from './components/Content/Content'
 const App = () => (
-    <Content />
+    <div className="app">
+        <div className="app__weather">
+            <Header />
+            <Content />
+        </div>
+    </div>
 )    
 
 export default App;
