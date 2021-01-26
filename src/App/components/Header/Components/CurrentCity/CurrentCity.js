38f34282a0939b8333d5ad1b7bcb69a62d79f558
Weixin Qi <s4590288@gmail.com>
2021-01-26 22:30:52 +1000

    fix: 🐛 currentWeather component's className problem
    change the className 'header__current' to 'current__weather' change the
    className 'current__detail' to 'weather__detail'

---
+++ src/App/components/Header/Components/CurrentCity/CurrentCity.js
@@ -0,0 +1,7 @@
+import './CurrentCity.scss';
+const CurrentCity = () => (
+    <div className="header__current">
+        <h1>Melbourne</h1>
+    </div>
+)
+export default CurrentCity;
--- src/App/components/Header/Components/CurrentWeather/CurrentWeather.js
+++ src/App/components/Header/Components/CurrentWeather/CurrentWeather.js
@@ -1,19 +1,19 @@
 import './CurrentWeather.scss'
 const CurrentWeather = () => (
-    <div className="header__current">
-        <div className="header__current--temp">
+    <div className="current__weather">
+        <div className="current__weather--temp">
             19.58 °
         </div>
-        <div className="header__current--weather">
+        <div className="current__weather--weather">
             Clouds
         </div>
-        <div className="header__current--detail">
-            <div className="current__detail--humidity">
+        <div className="current__weather--detail">
+            <div className="weather__detail--humidity">
                 <h4>HUMIDITY</h4>
                 <div className="detail__humidity--value">94%</div>
             </div>
-            <div className="current__detail--divider"></div>
-            <div className="current__detail--wind">
+            <div className="weather__detail--divider"></div>
+            <div className="weather__detail--wind">
                 <h4>WIND</h4>
                 <div className="detail__wind--speed">7.2K/M</div>
             </div>
